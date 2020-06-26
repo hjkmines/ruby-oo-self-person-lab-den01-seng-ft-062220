a4162f6f5b01c402d1458acfba1fd700a9b1f649
Hyung Kim <hjkmines@gmail.com>
2020-06-26 02:57:56 +0000

    Automatically backed up by Learn

--- lib/person.rb
+++ lib/person.rb
@@ -1,1 +1,15 @@
 # your code goes here
+class Person 
+  attr_accessor :money, :happiness, :hygiene
+  attr_reader :name 
+  
+  def initialize name 
+    @name = name 
+    @money = 25
+    @happiness = 8
+    @hygiene = 8
+  end 
+  
+  
+  
+end
